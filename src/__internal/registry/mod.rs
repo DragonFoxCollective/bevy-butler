93ca3ef2e6c36298b9d70e2d586db548e98ec390
TGRCDev <tgrc@tgrc.dev>
2025-01-13 22:07:35 -0800

    Okay mutually exclusive features don't fucking work, don't do them

--- src/__internal/registry/mod.rs
+++ src/__internal/registry/mod.rs
@@ -1,23 +1,18 @@
-#[cfg(all(feature="linkme", feature="inventory"))]
-compile_error!("Features \"linkme\" and \"inventory\" are mutually exclusive. To use \"inventory\", disable default features.");
-#[cfg(not(any(feature="linkme", feature="inventory")))]
-compile_error!("bevy-butler needs a registry backend to construct the plugin registry. Please enable \"linkme\" or \"inventory\".");
-
 use std::{collections::{HashMap, HashSet}, sync::LazyLock};
 use bevy_app::App;
 use std::any::TypeId;
 
-#[cfg(feature="linkme")]
+#[cfg(not(feature="inventory"))]
 mod backend_linkme;
-#[cfg(feature="linkme")]
+#[cfg(not(feature="inventory"))]
 pub use backend_linkme::*;
-#[cfg(feature="linkme")]
+#[cfg(not(feature="inventory"))]
 pub use ::linkme;
 
 #[cfg(feature="inventory")]
 mod backend_inventory;
 #[cfg(feature="inventory")]
-pub use backend_inventory::*;
+use backend_inventory::*;
 #[cfg(feature="inventory")]
 pub use ::inventory;
 
